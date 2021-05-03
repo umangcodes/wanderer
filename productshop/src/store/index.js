de23cc13b44347b4a28c5fb555bdd87a893ce05f
Umang A <umanga.eng@gmail.com>
2021-05-03 05:44:42 -0400

    events object added in store state

--- productshop/src/store/index.js
+++ productshop/src/store/index.js
@@ -1,8 +1,44 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
 
 export default createStore({
-  state: {},
+  state: {
+    events: [
+      {
+        id: 1,
+        category: 'C1',
+        title: 'CT1',
+        description: 'CD1',
+        location: 'CL1',
+        date: 'CD1',
+        time: 'CT1',
+        petsAllowed: true,
+        organizer: 'CO1',
+      },
+      {
+        id: 2,
+        category: 'C1',
+        title: 'CT1',
+        description: 'CD1',
+        location: 'CL1',
+        date: 'CD1',
+        time: 'CT1',
+        petsAllowed: true,
+        organizer: 'CO1',
+      },
+      {
+        id: 3,
+        category: 'C1',
+        title: 'CT1',
+        description: 'CD1',
+        location: 'CL1',
+        date: 'CD1',
+        time: 'CT1',
+        petsAllowed: true,
+        organizer: 'CO1',
+      },
+    ],
+  },
   mutations: {},
   actions: {},
   modules: {},
-});
+})
