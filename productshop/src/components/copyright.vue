d67ac0841923c862a33673fc754754cc550801ef
Umang A <umanga.eng@gmail.com>
2021-05-03 04:22:23 -0400

    copyright file with a prop added

---
+++ productshop/src/components/copyright.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © 2020 Copyright: {{ companyName }}
+      <!-- TODO: use store in future -->
+    </div>
+    <!-- Copyright -->
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {
+    companyName: {
+      type: String,
+    },
+  },
+}
+</script>
