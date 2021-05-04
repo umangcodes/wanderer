8a626d4c3c8d816285e90c07dddd738e8848559e
Umang A <umanga.eng@gmail.com>
2021-05-04 07:41:35 -0400

    eventlist now contains product cards

---
+++ productshop/src/views/EventList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <div class="cards">
+      <productCard v-for="event in $store.state.events" :key="event.id" :cardName="event.title" :cardId="event.id"/>
+      
+    </div>
+  </div>
+  <Footer />
+</template>
+
+<script>
+// @ is an alias to /src
+import productCard from '@/components/productCard.vue'
+import Footer from '../components/footer.vue'
+
+export default {
+  name: 'EventList',
+  components: {
+    productCard,
+    Footer,
+  },
+  data(){
+    return{
+      company : "SSE",
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+.cards {
+  display: flex;
+  /* flex-wrap: wrap; */
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  /* width: 500px; */
+  padding: 30px;
+}
+/* .home {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-items: center;
+} */
+</style>
