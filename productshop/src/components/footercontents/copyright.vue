28e9d16e8ab42f4c2b31dc3e0de058d759b7f261
Umang A <umanga.eng@gmail.com>
2021-05-04 07:06:40 -0400

    added scoped style for footer.

--- productshop/src/components/footercontents/copyright.vue
+++ productshop/src/components/footercontents/copyright.vue
@@ -1,16 +1,19 @@
 <template>
-  <footer class="bg-light text-center text-lg-start">
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-      © 2020 Copyright: {{ companyName }}
-      <!-- TODO: use store in future -->
-    </div>
-    <!-- Copyright -->
-  </footer>
+  <div class="copyright">
+    <footer class="bg-light text-center text-lg-start">
+      <!-- Copyright -->
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2020 Copyright: {{ companyName }}
+        <!-- TODO: use store in future -->
+      </div>
+      <!-- Copyright -->
+    </footer>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'Copyright',
   props: {
     companyName: {
       type: String,
@@ -18,3 +21,11 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.copyright {
+  position: absolute;
+  bottom: 0;
+  width: 100vh;
+}
+</style>
