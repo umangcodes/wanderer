cda96c95c73f4e6a3c9935ee84918649a69b4bf1
Umang A <umanga.eng@gmail.com>
2021-05-06 05:59:36 -0400

    productservices.js now provides an apiclient for fetching data from remote server, this service uses axios.

---
+++ productshop/src/services/ProductServices.js
@@ -0,0 +1,18 @@
+// This service handles all the api calls for fetching products data.
+
+import axios from 'axios'
+
+const clientApi = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/umangcodes/wanderer',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+export default {
+  getProducts() {
+    return clientApi.get('/events')
+  },
+}
