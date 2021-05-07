bf4e687d2626a3d81667de181677694c25e09118
Umang A <umanga.eng@gmail.com>
2021-05-07 08:58:03 -0400

    added getters and todos and events list

--- vuex/src/store.js
+++ vuex/src/store.js
@@ -17,8 +17,31 @@ export default new Vuex.Store({
       'education',
       'food',
       'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ],
+    events: [
+      { id: 1, text: '...', organizer: '...' },
+      { id: 2, text: '...', organizer: '...' },
+      { id: 3, text: '...', organizer: '...' },
+      { id: 4, text: '...', organizer: '...' }
     ]
   },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    activeTodosCount: state => {
+      return state.todos.filter(todos => !todos.done)
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
