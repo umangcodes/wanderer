2e13c6f9aa71e715840149c215bb2d465f5e0806
Umang A <umanga.eng@gmail.com>
2021-05-07 02:53:59 -0400

    added a home page to the project.

---
+++ vuex/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>
+            Platform under development... Sorry for inconveninence!!!
+        </h3>
+        <router-link :to="{name:'event-list'}">
+            <h6>Please click here for testing some development features.</h6>
+        </router-link>
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
