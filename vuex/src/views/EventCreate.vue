e15a6f74bcbd80a24db2ae446a90750bb4f624ee
Umang A <umanga.eng@gmail.com>
2021-05-08 07:14:50 -0400

    new code for create event view

--- vuex/src/views/EventCreate.vue
+++ vuex/src/views/EventCreate.vue
@@ -1,20 +1,38 @@
 <template>
   <div>
-    <h1>
-      Event by {{user.name}}
-    </h1>
-    <p>
-      This event is created by user with id {{user.id}}
-    </p>
-    <p>Total catergories: {{ totalCategories }}</p>
-    <span>{{ getEvent(1).id }}</span>
-    <br>
-    <span>text: {{ getEvent(1).text }}</span>
-    <br>
-    <span>organizer:{{ getEvent(1).organizer }}</span>
-    <ul>
-      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
-    </ul>
+  <h1>Your event</h1>
+  <form @submit.prevent="createEvent">
+          <label>Select a category</label>
+          <select v-model="event.category">
+            <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+          </select>
+          <h3>Name & describe your event</h3>
+          <div class="field">
+            <label>Title</label>
+            <input v-model="event.title" type="text" placeholder="Add an event title"/>
+          </div>
+          <div class="field">
+            <label>Description</label>
+            <input v-model="event.description" type="text" placeholder="Add a description"/>
+          </div>
+          <h3>Where is your event?</h3>
+          <div class="field">
+            <label>Location</label>
+            <input v-model="event.location" type="text" placeholder="Add a location"/>
+          </div>
+          <h3>When is your event?</h3>
+          <div class="field">
+            <label>Date</label>
+            <datepicker v-model="event.date" placeholder="Select a date"/>
+          </div>
+          <div class="field">
+            <label>Select a time</label>
+            <select v-model="event.time">
+              <option v-for="time in times" :key="time">{{ time }}</option>
+            </select>
+          </div>
+          <input type="submit" class="button -fill-gradient" value="Submit"/>
+        </form>
   </div>
 </template>
 
@@ -22,8 +40,27 @@
 import { mapState } from 'vuex'
 export default {
   methods: {
+    createEvent() {
+      this.$store.dispatch('createEvent', this.event)
+    },
     incrementCount() {
       this.store.dispatch('updateCount', 2)
+    },
+    createFreshUserObject() {
+      const user = this.$store.state.user
+      const id = Math.floor(Math.random() * 1000000)
+
+      return {
+        id: id,
+        category: '',
+        organizer: user,
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
     }
   },
   computed: {
@@ -37,6 +74,17 @@ export default {
       user: 'user',
       categories: 'categories'
     })
+  },
+  data() {
+    const times = []
+    for (let i = 1; i < 24; i++) {
+      times.push(i + ':00')
+    }
+    return {
+      categories: this.$store.state.categories,
+      times,
+      event: this.createFreshEventObject()
+    }
   }
 }
 </script>
