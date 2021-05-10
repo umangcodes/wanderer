3ca036ee644add0d267bb5990582450d6be63b50
Umang A <umanga.eng@gmail.com>
2021-05-10 07:55:25 -0400

    store modularized

--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import EventService from '../services/EventService'
+
+import * as event from '@/store/modules/event.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    event
+  },
   state: {
     count: 0,
     user: {
@@ -24,59 +28,6 @@ export default new Vuex.Store({
       { id: 2, text: '...', done: false },
       { id: 3, text: '...', done: true },
       { id: 4, text: '...', done: false }
-    ],
-    events: [],
-    totalEvents: 0,
-    event: {}
-  },
-  mutations: {
-    ADD_EVENT(state, event) {
-      state.event.push(event)
-    },
-    SET_EVENTS(state, response) {
-      state.events = response.data
-      state.totalEvents = response.headers['x-total-count']
-    },
-    SET_EVENT(state, event) {
-      state.event = event
-    }
-  },
-  actions: {
-    createEvent(commit, event) {
-      return EventService.postEvent(event).then(() =>
-        commit('ADD_EVENT', event)
-      )
-    },
-    fetchEvents({ state, commit }, { perPage, page }) {
-      EventService.getEvents(perPage, page)
-        .then(response => {
-          state.totalEvents = response.headers['x-total-count']
-          console.log(response.headers['x-total-count'])
-          commit('SET_EVENTS', response)
-        })
-        .catch(error => {
-          console.log('There was an error:', error.response)
-        })
-    },
-    fetchEvent({ commit, getters }, id) {
-      var event = getters.getEventById(id)
-      if (event) {
-        commit('fetchEvent', event)
-      } else {
-        EventService.getEvent(id)
-          .then(response => {
-            console.log(response.data)
-            commit('SET_EVENT', response.data)
-          })
-          .catch(error => {
-            console.log('There was an error:', error.response)
-          })
-      }
-    }
-  },
-  getters: {
-    getEventById: state => id => {
-      return state.events.find(event => event.id === id)
-    }
+    ]
   }
 })
