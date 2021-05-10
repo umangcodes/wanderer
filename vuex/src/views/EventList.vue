681e1f3bfc541578b781d560625fef3ad886e5e8
Umang A <umanga.eng@gmail.com>
2021-05-10 00:37:46 -0400

    computed properties for pagination added. disptach action changed, now passing two additional params for pagination. router links added for previous and next page.

--- vuex/src/views/EventList.vue
+++ vuex/src/views/EventList.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <template v-if="page != 1">
+    <router-link  :to="{ name: 'event-list', query: {page: page-1}}" rel="prev"> previous page </router-link>|
+    </template>
+    <router-link :to="{ name: 'event-list', query: {page: page+1}}" rel="next"> next page </router-link>
+    <span>Current Page: {{$route.query.page}}</span>/
+    <span> {{Math.floor($store.state.totalEvents / this.perPage) + 1}}</span>
   </div>
 </template>
 
@@ -14,10 +20,21 @@ export default {
     EventCard
   },
   created() {
-    this.$store.dispatch('fetchEvents')
+    this.$store.dispatch('fetchEvents', {
+      perPage: this.perPage,
+      page: this.page
+    })
   },
-  computed: mapState({
-    events: state => state.events
-  })
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    perPage() {
+      return 3
+    },
+    ...mapState({
+      events: state => state.events
+    })
+  }
 }
 </script>
