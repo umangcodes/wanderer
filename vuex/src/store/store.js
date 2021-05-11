9cc7228bf486ee5e23acb13cd22e7cd21f8fdff3
Umang A <umanga.eng@gmail.com>
2021-05-11 06:11:15 -0400

    notifications module imported  to the root store and registered as a module

--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as user from '@/store/modules/user.js'
 import * as event from '@/store/modules/event.js'
+
+import * as notification from '@/store/modules/notifications.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     event,
-    user
+    user,
+    notification
   },
   state: {
     count: 0,
