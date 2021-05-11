94f553c932da5909f8b9154ed11c9216c7846f40
Umang A <umanga.eng@gmail.com>
2021-05-11 06:35:55 -0400

    identation changed

--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as user from '@/store/modules/user.js'
 import * as event from '@/store/modules/event.js'
+import * as notification from '@/store/modules/notification.js'
 
-import * as notification from '@/store/modules/notifications.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
