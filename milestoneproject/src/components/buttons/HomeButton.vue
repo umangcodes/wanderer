a6463309d737f7a097ff02ee67a015abd2aebb68
Umang A <umanga.eng@gmail.com>
2021-05-13 13:07:33 -0400

    home button center align issue resolved. on hover darken shadow and background effect added. router link had a div with class home was removed

--- milestoneproject/src/components/buttons/HomeButton.vue
+++ milestoneproject/src/components/buttons/HomeButton.vue
@@ -1,17 +1,15 @@
 <template>
-  <router-link :to="{ name: 'Home' }" class="float">
-    <div class="home">Home</div>
-  </router-link>
+  <router-link :to="{ name: 'Home' }" class="float home"> Home </router-link>
 </template>
 
 <style scoped>
 .float {
   position: fixed;
   width: 60px;
-  height: auto;
-  bottom: 40px;
-  right: 40px;
-  background-color: #0c9;
+  height: 15px;
+  bottom: 10px;
+  right: 20px;
+  background-color: rgba(0, 204, 153, 1);
   color: #fff;
   border-radius: 50px;
   box-shadow: 2px 2px 3px #999;
@@ -21,10 +19,14 @@
   text-align: center;
 }
 
+.float:hover {
+  transition: 0.4s;
+  background-color: rgba(0, 184, 133, 1);
+  box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.75);
+}
+
 .home {
-  text-align: center;
   font-size: 20px;
   font-style: italic;
-  font-weight: 900;
 }
 </style>
