b6ccee8caa8d51d6e66ecbc6406ed1c7f59be632
Umang A <umanga.eng@gmail.com>
2021-05-13 11:50:23 -0400

    component homebutton created. this button is a floating button with router link named path to home page

---
+++ milestoneproject/src/components/buttons/HomeButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-link :to="{ name: 'Home' }" class="float">
+    <div class="home">Home</div>
+  </router-link>
+</template>
+
+<style scoped>
+.float {
+  position: fixed;
+  width: 60px;
+  height: auto;
+  bottom: 40px;
+  right: 40px;
+  background-color: #0c9;
+  color: #fff;
+  border-radius: 50px;
+  box-shadow: 2px 2px 3px #999;
+  padding: 20px;
+  margin: 10px;
+  text-decoration: none;
+  text-align: center;
+}
+
+.home {
+  text-align: center;
+  font-size: 20px;
+  font-style: italic;
+  font-weight: 900;
+}
+</style>
