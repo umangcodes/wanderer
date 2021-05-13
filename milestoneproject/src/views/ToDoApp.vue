84955d58e17014243fc006f55c6d92481d05e001
Umang A <umanga.eng@gmail.com>
2021-05-13 11:49:06 -0400

    added to new views for weather app and to do list

---
+++ milestoneproject/src/views/ToDoApp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    <h1>Todo list</h1>
+    <HomeButton />
+  </div>
+</template>
+
+<script>
+import HomeButton from "@/components/buttons/HomeButton.vue";
+export default {
+  components: {
+    HomeButton,
+  },
+};
+</script>
---
+++ milestoneproject/src/views/WeatherApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <div class="home">
+      <h1>Weather App View</h1>
+    </div>
+    <HomeButton />
+  </div>
+</template>
+
+<script>
+import HomeButton from "@/components/buttons/HomeButton.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HomeButton,
+  },
+};
+</script>
