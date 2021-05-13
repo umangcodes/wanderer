ecbdf8d3567a48e0f26493a3745a294cae5519de
Umang A <umanga.eng@gmail.com>
2021-05-13 17:48:09 -0400

    the .json method is promise based, the function needs to wait till the promise is fulfilled.

--- milestoneproject/src/services/weatherService.js
+++ milestoneproject/src/services/weatherService.js
@@ -1,10 +1,11 @@
 export default {
   async getWeather(url, location, apiKey) {
-    await fetch(
-      `${url}weather?q=${location}&units=metric&APPID=${apiKey}`
-    ).then((response) => {
-      console.log(response);
-      return response.json();
-    });
+    await fetch(`${url}weather?q=${location}&units=metric&APPID=${apiKey}`)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        return res;
+      });
+    return data;
   },
 };
