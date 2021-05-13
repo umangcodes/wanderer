8554c9c45b1e2a6d59fc22acf5d596dcdae2b944
Umang A <umanga.eng@gmail.com>
2021-05-13 17:05:00 -0400

    added location apikey and baseurl information to make api call

--- milestoneproject/src/store/index.js
+++ milestoneproject/src/store/index.js
@@ -2,7 +2,9 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    location: '',
+    location: "london",
+    apiKey: "7f155d2ca027ace846f8991f680697de",
+    baseUrl: "https://api.openweathermap.org/data/2.5/",
   },
   mutations: {},
   actions: {},
