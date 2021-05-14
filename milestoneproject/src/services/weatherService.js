bce417c595b39c4ab3cc924c32d8178529882f59
Umang A <umanga.eng@gmail.com>
2021-05-14 00:52:45 -0400

    weatherservices method getweather modified to get data.

--- milestoneproject/src/services/weatherService.js
+++ milestoneproject/src/services/weatherService.js
@@ -1,10 +1,10 @@
 export default {
   async getWeather(url, location, apiKey) {
-    await fetch(`${url}weather?q=${location}&units=metric&APPID=${apiKey}`)
-      .then((response) => response.json())
-      .then((res) => {
-        console.log(res);
-        return res;
-      });
+    const data = await fetch(
+      `${url}weather?q=${location}&units=metric&APPID=${apiKey}`
+    ).then((response) => {
+      return response.json();
+    });
+    return data;
   },
 };
