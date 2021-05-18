bac367d372f016748ffe9a81af016dea25b698e6
Umang A <umanga.eng@gmail.com>
2021-05-18 14:44:33 -0400

    css changed to gray background along with some changes to box shadow

--- milestoneproject/src/components/buttons/HomeButton.vue
+++ milestoneproject/src/components/buttons/HomeButton.vue
@@ -9,10 +9,10 @@
   height: Auto;
   bottom: 10px;
   right: 20px;
-  background-color: rgba(0, 204, 153, 1);
+  background-color: rgba(61, 61, 61, 1);
   color: #fff;
   border-radius: 50px;
-  box-shadow: 2px 2px 3px #999;
+  box-shadow: 1px rgba(61, 61, 61, 1);
   padding: 20px;
   margin: 10px;
   text-decoration: none;
@@ -21,7 +21,7 @@
 
 .float:hover {
   transition: 0.4s;
-  background-color: rgba(0, 184, 133, 1);
+  background-color: rgba(41, 41, 41, 0.5);
   box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.75);
 }
 
