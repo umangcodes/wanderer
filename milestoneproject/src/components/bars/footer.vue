509994e4988b213121d0d365e451fb15fb1174bb
Umang A <umanga.eng@gmail.com>
2021-05-18 18:10:47 -0400

    added footer file.

---
+++ milestoneproject/src/components/bars/footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h4>Developed By Umang A</h4>
+    <p>https://github.com/umangcodes</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
