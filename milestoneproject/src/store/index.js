f8c1735ea56b57774a37ac5c0a5ae38839fde186
Umang A <umanga.eng@gmail.com>
2021-05-18 23:06:14 -0400

    added state, mutations and an action to manipulate the state.

--- milestoneproject/src/store/index.js
+++ milestoneproject/src/store/index.js
@@ -1,13 +1,35 @@
 import { createStore } from "vuex";
-
+import weatherService from "@/services/weatherService";
 export default createStore({
   state: {
     location: "london",
     apiKey: "7f155d2ca027ace846f8991f680697de",
     baseUrl: "https://api.openweathermap.org/data/2.5/",
     weather: {},
+    display: false,
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    UPDATE_DATA(state, data) {
+      state.weather = data;
+    },
+    UPDATE_DISPLAY(state) {
+      state.display = true;
+    },
+  },
+  actions: {
+    weatherUpdate({ state, commit }, query) {
+      state.location = query;
+      console.log(state.location);
+
+      weatherService
+        .getWeather(state.baseUrl, query, state.apiKey)
+        .then((response) => {
+          commit("UPDATE_DATA", response);
+          commit("UPDATE_DISPLAY");
+          console.log("updatedisplay");
+          console.log(state.display);
+        });
+    },
+  },
   modules: {},
 });
