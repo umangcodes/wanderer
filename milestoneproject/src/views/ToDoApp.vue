6b945834203d7228934555a401b70e473ad875fd
Umang A <umanga.eng@gmail.com>
2021-05-19 18:18:43 -0400

    added task form to the view

--- milestoneproject/src/views/ToDoApp.vue
+++ milestoneproject/src/views/ToDoApp.vue
@@ -2,14 +2,17 @@
   <div class="page">
     <h1>Todo list</h1>
     <HomeButton />
+    <TaskForm />
   </div>
 </template>
 
 <script>
 import HomeButton from "@/components/buttons/HomeButton.vue";
+import TaskForm from "@/components/panels/taskForm.vue";
 export default {
   components: {
     HomeButton,
+    TaskForm,
   },
 };
 </script>
