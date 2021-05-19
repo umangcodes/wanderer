006aa8941bde8ff5dea6331902ca69da4919d0f2
Umang A <umanga.eng@gmail.com>
2021-05-19 18:18:00 -0400

    added form along with the event listener which calls formSubmit method. even when form is submitted, it calls the same method

--- milestoneproject/src/components/panels/taskForm.vue
+++ milestoneproject/src/components/panels/taskForm.vue
@@ -1,9 +1,66 @@
 <template>
-  <div></div>
+  <div class="to-do-app">
+    <form @submit.prevent="formSubmit">
+      <div>
+        <h3>Task</h3>
+        <input
+          type="text"
+          placeholder="Please enter task name"
+          v-model="task"
+        />
+        {{ task }}
+      </div>
+      <div>
+        <h3>Category</h3>
+        <select name="category" v-model="category">
+          <option value="cat-1">Cat 1</option>
+          <option value="cat-2">Cat 2</option>
+          <option value="cat-3">Cat 3</option>
+          <option value="cat-4">Cat 4</option>
+        </select>
+        <span>{{ category }}</span>
+      </div>
+      <div>
+        <h3>Description</h3>
+        <input
+          type="text"
+          placeholder="Please enter task description"
+          v-model="description"
+        />
+        {{ description }}
+      </div>
+      <Submit :submitPressed="formSubmit" />
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+import Submit from "@/components/buttons/submit.vue";
+export default {
+  name: "taskForm",
+  components: {
+    Submit,
+  },
+  data() {
+    return {
+      task: "",
+      description: "",
+      category: "cat",
+    };
+  },
+  methods: {
+    formSubmit() {
+      console.log(this.category);
+    },
+  },
+  created() {
+    console.log(this.category);
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.to-do-app {
+  color: white;
+}
+</style>
