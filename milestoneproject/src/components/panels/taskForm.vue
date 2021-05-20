2c2764079896ccf817ee1387f2f3bf5116fc6923
Umang A <umanga.eng@gmail.com>
2021-05-19 23:56:59 -0400

    task form modified to accomodte the submit button along with the submit method. Form now also has the toggle button to toggle the task add display

--- milestoneproject/src/components/panels/taskForm.vue
+++ milestoneproject/src/components/panels/taskForm.vue
@@ -1,60 +1,96 @@
 <template>
   <div class="to-do-app">
-    <form @submit.prevent="formSubmit">
-      <div class="task">
-        <h3>Task</h3>
-        <input
-          type="text"
-          placeholder="Please enter task name"
-          v-model="task"
+    <div class="toggle-button">
+      <CustomButton
+        label="X"
+        emitEvent="closePanel"
+        @closePanel="toggleAddTaskPanel"
+      />
+    </div>
+    <div v-if="toggleTaskPanel != false">
+      <form @submit.prevent="formSubmit">
+        <div class="task">
+          <h3>Task</h3>
+          <input
+            type="text"
+            placeholder="Please enter task name"
+            v-model="task"
+          />
+          {{ task }}
+        </div>
+        <div class="task category">
+          <h3>Category</h3>
+          <select name="category" v-model="category">
+            <option value="cat-1">Cat 1</option>
+            <option value="cat-2">Cat 2</option>
+            <option value="cat-3">Cat 3</option>
+            <option value="cat-4">Cat 4</option>
+          </select>
+          <span>{{ category }}</span>
+        </div>
+        <div class="task description">
+          <h3>Description</h3>
+          <input
+            type="text"
+            placeholder="Please enter task description"
+            v-model="description"
+          />
+          {{ description }}
+        </div>
+        <div class="task mark">
+          <h3 style="padding: 0px">Do you want me to mark it?</h3>
+          <input
+            style="height: 25px; width: 25px"
+            type="checkbox"
+            true-false="yes"
+            v-model="remind"
+          />
+          {{ description }}
+        </div>
+        <Submit :submitPressed="formSubmit" />
+        <CustomButton
+          label="Button 1"
+          emitEvent="button1"
+          :button1="buttonPressed"
         />
-        {{ task }}
-      </div>
-      <div class="task category">
-        <h3>Category</h3>
-        <select name="category" v-model="category">
-          <option value="cat-1">Cat 1</option>
-          <option value="cat-2">Cat 2</option>
-          <option value="cat-3">Cat 3</option>
-          <option value="cat-4">Cat 4</option>
-        </select>
-        <span>{{ category }}</span>
-      </div>
-      <div class="task description">
-        <h3>Description</h3>
-        <input
-          type="text"
-          placeholder="Please enter task description"
-          v-model="description"
-        />
-        {{ description }}
-      </div>
-      <Submit :submitPressed="formSubmit" />
-    </form>
+      </form>
+    </div>
+    <div v-if="toggleTaskPanel == false">
+      <h1>Press button to add tasks</h1>
+    </div>
   </div>
 </template>
 
 <script>
 import Submit from "@/components/buttons/submit.vue";
+import CustomButton from "@/components/buttons/customButton.vue";
 export default {
   name: "taskForm",
   components: {
     Submit,
+    CustomButton,
   },
   data() {
     return {
       task: "",
       description: "",
-      category: "cat",
+      category: "",
+      remind: "",
+      toggleTaskPanel: false,
     };
   },
   methods: {
     formSubmit() {
       console.log(this.category);
+      console.log(this.remind);
     },
-  },
-  created() {
-    console.log(this.category);
+    buttonPressed() {
+      console.log("Button is pressed");
+    },
+    toggleAddTaskPanel() {
+      this.toggleTaskPanel = !this.toggleTaskPanel;
+      console.log(this.toggleTaskPanel);
+    },
   },
 };
 </script>
@@ -64,13 +100,19 @@ export default {
   width: 500px;
   height: auto;
   border: 2px solid white;
+  border-radius: 15px;
   color: white;
   padding: 10px;
   padding-top: 0px;
   margin: 10px;
 }
+
 .task {
   font-family: "Segoe UI";
   font-size: 20px;
+}
+.toggle-button {
+  text-align: right;
+  padding: 10px;
 }
 </style>
