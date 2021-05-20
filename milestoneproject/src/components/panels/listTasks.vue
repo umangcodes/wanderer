5deef758b98c2de5083cd6edff31eb3e9dc9a3f9
Umang A <umanga.eng@gmail.com>
2021-05-20 17:54:59 -0400

    close button component added to the file

--- milestoneproject/src/components/panels/listTasks.vue
+++ milestoneproject/src/components/panels/listTasks.vue
@@ -1,12 +1,53 @@
 <template>
-  <div class="task">
-    <h3>task name</h3>
-    <p>task description</p>
+  <div class="task" v-for="task in tasks" :key="task.id">
+    <div class="task-display" :class="task.reminder ? 'highlight' : ''">
+      <div class="text-box">
+        <h3>{{ task.text }}</h3>
+        <p>{{ task.day }}</p>
+      </div>
+      <CustomButton
+        label="X"
+        emitEvent="closeButton"
+        @closeButton="closeClicked(task.id)"
+        style="padding: 10px"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import CustomButton from "@/components/buttons/customButton.vue";
+export default {
+  components: {
+    CustomButton,
+  },
+  props: {
+    tasks: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    closeClicked(id) {
+      console.log(id);
+    },
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.task-display {
+  display: flex;
+  border: 1px solid white;
+  width: 100vh;
+  padding-left: 30px;
+  margin: 10px;
+  border-radius: 20px;
+}
+.highlight {
+  border-left: 5px solid green;
+}
+.text-box {
+  width: 500px;
+}
+</style>
