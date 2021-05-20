fbda49dea62808cdf21906fcb3d7ae9f5391500d
Umang A <umanga.eng@gmail.com>
2021-05-20 17:04:45 -0400

    added class binding to remind the task

--- milestoneproject/src/views/ToDoApp.vue
+++ milestoneproject/src/views/ToDoApp.vue
@@ -3,16 +3,26 @@
     <h1>Todo list</h1>
     <HomeButton />
     <TaskForm />
+    <div class="task-display" :class="remind ? 'highlight' : ''">
+      <ListTasks />
+    </div>
   </div>
 </template>
 
 <script>
 import HomeButton from "@/components/buttons/HomeButton.vue";
 import TaskForm from "@/components/panels/taskForm.vue";
+import ListTasks from "@/components/panels/listTasks.vue";
 export default {
   components: {
     HomeButton,
     TaskForm,
+    ListTasks,
+  },
+  data() {
+    return {
+      remind: true,
+    };
   },
 };
 </script>
@@ -21,5 +31,15 @@ export default {
 .page {
   height: auto;
   min-height: 500px;
+}
+.task-display {
+  border: 1px solid white;
+  width: 100vh;
+  padding-left: 30px;
+  margin: 10px;
+  border-radius: 20px;
+}
+.highlight {
+  border-left: 5px solid green;
 }
 </style>
