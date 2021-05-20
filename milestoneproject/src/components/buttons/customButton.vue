1e976503516c43099315cafc23cd1fa22d5f9d14
Umang A <umanga.eng@gmail.com>
2021-05-19 23:54:07 -0400

    added a custom button component. This component takes in label and name of event to emit when triggered as prop. the component emits the event when click. To change css, out this in a separate div

---
+++ milestoneproject/src/components/buttons/customButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button v-on:click.prevent="buttonPress(emitEvent)">{{ label }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "customButton",
+  props: {
+    emitEvent: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    buttonPress(event) {
+      this.$emit(event);
+      console.log("button pressed");
+      console.log(event);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
