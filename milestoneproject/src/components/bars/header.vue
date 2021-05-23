59b601717bd66a3a2c8e463a221004e616205f65
Umang A <umanga.eng@gmail.com>
2021-05-22 21:04:12 -0400

    header added

---
+++ milestoneproject/src/components/bars/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <div class="box">
+      <div class="logo">
+        <h1>UA</h1>
+      </div>
+      <div class="title">
+        <h1>Portfolio Project</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  margin-top: 10px;
+  padding: 10px 10px 10px 50px;
+  border: 1px solid black;
+}
+.logo {
+  padding: 20px;
+}
+.title {
+  padding: 20px;
+}
+h1 {
+  margin: 0px;
+}
+.box {
+  display: flex;
+  /* 
+  border-bottom: 2px solid rgba(90, 169, 230, 1);
+  max-width: calc(100vw / 2);*/
+}
+</style>
