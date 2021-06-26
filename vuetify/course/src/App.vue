a189d086004b950e9b2f1b1b5038b17c76ded269
Umang A <umanga.eng@gmail.com>
2021-06-26 16:27:43 -0400

    code refactored

--- vuetify/course/src/App.vue
+++ vuetify/course/src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <v-app>
-    <!-- <v-app-bar app color="#0E2383" dark>
-      <v-toolbar-title>Course @ Vuemastery</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn> -->
-    <!-- </v-app-bar> -->
     <Header />
     <v-main>
       <router-view />
