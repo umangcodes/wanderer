7f67ec6acbc16017d16e7ddd1f885b24c330bdd0
Umang A <umanga.eng@gmail.com>
2021-06-26 16:29:00 -0400

    links added using links object

--- vuetify/course/src/components/header.vue
+++ vuetify/course/src/components/header.vue
@@ -2,13 +2,32 @@
   <v-app-bar app color="#0E2383" dark>
     <v-toolbar-title>Course @ Vuemastery</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn text rounded>Home</v-btn>
-    <v-btn text rounded>Login</v-btn>
+    <v-btn
+      text
+      rounded
+      v-for="link in links"
+      :key="`${link.label}-header-link`"
+      :to="link.url"
+    >
+      {{ link.label }}</v-btn
+    >
+
+    <!-- :to="{ name: 'Home' }" -->
+    <!-- :to="{ name: 'Login' }" -->
   </v-app-bar>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      links: [
+        { label: "Home", url: "/home" },
+        { label: "Login", url: "/" },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped></style>
