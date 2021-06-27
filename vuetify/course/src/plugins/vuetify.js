39a181a87b1feb732d57d00f9cfd1e4c13048190
Umang A <umanga.eng@gmail.com>
2021-06-27 00:26:50 -0400

    theme added

--- vuetify/course/src/plugins/vuetify.js
+++ vuetify/course/src/plugins/vuetify.js
@@ -3,4 +3,15 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#7c0024",
+        secondary: "#b0bec5",
+        anchor: "#8c9eff",
+        text: "#f7ece1",
+      },
+    },
+  },
+});
