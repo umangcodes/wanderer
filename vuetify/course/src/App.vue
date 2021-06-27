1d6b95a16827b49300c834a0c724c90235be20ae
Umang A <umanga.eng@gmail.com>
2021-06-27 00:24:32 -0400

    added css for glassmorph

--- vuetify/course/src/App.vue
+++ vuetify/course/src/App.vue
@@ -22,3 +22,16 @@ export default {
   }),
 };
 </script>
+
+<style>
+#app {
+  background: #7c0023e8;
+}
+.cardColor {
+  background-color: rgba(255, 255, 255, 0.1) !important;
+  border-color: white !important;
+  box-shadow: rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  color: #007c59;
+}
+</style>
