cffb74fe5fc853fd245c0209c8e7f65531c9f3fc
Umang A <umanga.eng@gmail.com>
2021-06-29 00:35:20 -0400

    modified about

--- vuetify/course/src/views/About.vue
+++ vuetify/course/src/views/About.vue
@@ -5,18 +5,15 @@
         <v-card class="triadicGlass" elevation="5" hover="true">
           <v-card-title> About Project: </v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
-            officia dolore officiis tempore id impedit adipisci expedita minus
-            laudantium inventore fugiat nemo eligendi asperiores ipsum doloribus
-            optio maiores necessitatibus nam assumenda perspiciatis nostrum, in
-            mollitia dolor! Aliquam a excepturi natus quia voluptate iure, sint
-            maxime nemo minus similique. Neque cum, natus quam quibusdam omnis
-            iusto eos ullam! Similique, itaque voluptatem exercitationem impedit
-            autem beatae obcaecati esse doloribus cum, nulla illo nobis
-            delectus, eius hic veritatis eveniet! Provident autem voluptas
-            tempore expedita eos beatae est voluptatibus dignissimos corrupti
-            temporibus, earum quaerat fuga cupiditate nisi architecto odit
-            reiciendis maxime sequi laborum porro.
+            Demo project created by Umang A. This project is developed using
+            vuetify components. The project does not use any backend tech and is
+            solely for the purpose of demonstration. For more details visit
+            <a href="https://vuetifyjs.com/en/" style="text-decoration: none">
+              Vuetify</a
+            >. For theme, colors and icons please visit
+            <a href="https://material.io">MD</a>.
+            <br />
+            For any more details please contact the developer.
           </v-card-text>
         </v-card>
       </v-col>
@@ -24,35 +21,45 @@
     <v-row>
       <v-col cols="12" md="6" lg="4">
         <v-card class="compGlass">
-          <v-card-title> Card 1 </v-card-title>
-          <v-card-text
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-            molestiae vel ullam, qui quam quod non amet. Inventore, corrupti
-            sapiente.</v-card-text
+          <v-card-title> Latest changes </v-card-title>
+          <v-card-text>
+            <h3>Register module</h3>
+            <ul>
+              <li>Form validation</li>
+              <li>Form actions</li>
+              <li>Form spacing [removed redun. grid comp]</li>
+            </ul></v-card-text
           >
         </v-card>
       </v-col>
       <v-col cols="12" md="6" lg="4">
         <v-card class="anaGlass">
-          <v-card-title> Card 2 </v-card-title>
-          <v-card-text
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-            molestiae vel ullam, qui quam quod non amet. Inventore, corrupti
-            sapiente.</v-card-text
+          <v-card-title> Git changes </v-card-title>
+          <v-card-text>
+            <h3><v-icon>mdi-github</v-icon> GitHub</h3>
+            <ul>
+              <li>active branch:lesson 7</li>
+              <li>staging branch: staging</li>
+              <li>dep branch: main</li>
+            </ul></v-card-text
           >
         </v-card>
       </v-col>
       <v-col cols="12" md="6" lg="4">
         <v-card class="compGlass">
-          <v-card-title> Card 3 </v-card-title>
-          <v-card-text
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-            molestiae vel ullam, qui quam quod non amet. Inventore, corrupti
-            sapiente.</v-card-text
+          <v-card-title> Deployment </v-card-title>
+          <v-card-text>
+            <ul>
+              <li>platform: vercel</li>
+              <li>branch: main</li>
+            </ul></v-card-text
           >
         </v-card>
       </v-col>
     </v-row>
+    <v-row
+      ><v-col><h1>Random stuff</h1></v-col></v-row
+    >
     <v-row>
       <v-col cols="3"></v-col>
       <v-col cols="6">
