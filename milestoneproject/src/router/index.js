872d6fa7cb0881a3ac07051eb251cff699cb9aeb
Umang A <umanga.eng@gmail.com>
2021-07-18 17:06:47 -0400

    routes updated

--- milestoneproject/src/router/index.js
+++ milestoneproject/src/router/index.js
@@ -2,28 +2,28 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "@/views/About.vue";
 import WeatherApp from "@/views/WeatherApp.vue";
-import ToDoApp from "@/views/ToDoApp.vue"
+import ToDoApp from "@/views/ToDoApp.vue";
 const routes = [
+  // {
+  //   path: "/",
+  //   name: "Home",
+  //   component: Home,
+  // },
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/WeatherApp",
     name: "WeatherApp",
     component: WeatherApp,
   },
-  {
-    path: "/ToDoApp",
-    name: "ToDo",
-    component: ToDoApp,
-  },
-  {
-    path: "/About",
-    name: "About",
-    component: About,
-  },
+  // {
+  //   path: "/ToDoApp",
+  //   name: "ToDo",
+  //   component: ToDoApp,
+  // },
+  // {
+  //   path: "/About",
+  //   name: "About",
+  //   component: About,
+  // },
 ];
 
 const router = createRouter({
